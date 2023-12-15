46889dc666536df9d81d16901b9704d3ccb4bc75
Tarık Özgün Güner <ozgunguner_99@yahoo.com>
2023-12-15 16:34:43 +0300

    Hepsiburada shopping Automation Test

---
+++ Hepsiburada.java
@@ -0,0 +1,81 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import java.time.Duration;
+import java.util.List;
+import java.util.Set;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Hepsiburada {
+
+    public static void main(String[] args) throws InterruptedException {
+        WebDriverManager.chromedriver();
+        WebDriver driver= new ChromeDriver();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        Actions actions = new Actions(driver); 
+        driver.get("https://www.hepsiburada.com/");
+        driver.manage().window().maximize();
+        WebElement popup=driver.findElement(By.xpath("//button[@id='onetrust-accept-btn-handler']"));
+        popup.click();
+
+
+         List<WebElement> menuElements = driver.findElements(By.xpath("//li[@class='sf-MenuItems-WulWXvlfIAwNiOUGY7FP']"));
+           
+        int indexToClick = 5; 
+        menuElements.get(indexToClick - 1).click(); 
+
+        WebElement thermomethers=driver.findElement(By.className("item-1169"));
+        actions.moveToElement(thermomethers).build().perform();
+        thermomethers.click();
+
+        WebElement xxx=driver.findElement(By.xpath("//label[@class='horizontalSortingBar-Ce404X9mUYVCRa5bjV4D']"));
+        xxx.click();
+        
+         List<WebElement> sortElements = driver.findElements(By.xpath("//a[@class='horizontalSortingBar-wxPx75gvwcKxOM8rMngA']"));
+        int indexToClick2 = 7;
+        sortElements.get(indexToClick2 - 1).click();
+
+        
+        WebElement element = wait.until(ExpectedConditions.elementToBeClickable(By.id("i12")));
+        element.click(); 
+        
+        
+
+        String anaPencere = driver.getWindowHandle();
+        Set<String> tumPencereler = driver.getWindowHandles();
+        for (String pencere : tumPencereler) {
+            if (!pencere.equalsIgnoreCase(anaPencere)) {
+                // Yeni sekmenin referansını saklayın ve o sekmeye geçiş yapın
+                driver.switchTo().window(pencere);
+                break;
+            }
+        }
+        WebElement addToBasket=driver.findElement(By.xpath("//button[@id='addToCart']"));
+        addToBasket.click();
+        WebElement goingOnShopping=driver.findElement(By.xpath("//button[@kind='secondary']"));
+        goingOnShopping.click();
+        WebElement Basket=driver.findElement(By.id("shoppingCart"));
+        Basket.click();
+        WebElement amount=driver.findElement(By.xpath("//input[@type='number']"));
+         amount.sendKeys(Keys.CONTROL+"a");
+         amount.sendKeys(Keys.DELETE);     
+         amount.sendKeys("100");
+         amount.click();
+        WebElement sendToTrash=driver.findElement(By.className("trash_button_3KWju"));
+        sendToTrash.click();
+        driver.close();
+
+//
+
+        
+     
+    
+}
+    
+
+}
---
+++ ReadMe.md
@@ -0,0 +1,24 @@
+# Hepsiburada Selenium Test Automation
+
+This project is an example that performs a series of automation tests on the Hepsiburada website using Selenium WebDriver.
+
+## Project Description
+
+This project automates a test scenario on the Hepsiburada website using Selenium WebDriver and the Java programming language:
+
+-   Step 1. Go to the home page.
+-   Step 2. Close the popup window that appears.
+-   Step 3. Navigate to a specific menu and click on a subcategory within it.
+-   Step 4. Select a product from the product list.
+-   Step 5. Change the order of the products by using the drop-down menu of the field where the products are sorted.
+-   Step 6. Select a specific product and add it to the cart.
+-   Step 7. Go to the cart page, increase the number of products and then delete the product from the cart.
+
+## Requirements
+
+The following components are needed to run this project:
+
+- Java
+- Selenium WebDriver
+- ChromeDriver
+- Maven (optionally available)
